bebd1ee84ff11bed0ed67eaacc8fe4e29e044b4e
johnebri <john.ebri@yahoo.com>
2020-01-23 23:15:15 +0000

    spring security

--- src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/SpringBootFirstWebApplication.java
+++ src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/SpringBootFirstWebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootFirstWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstWebApplication.class, args);
+		SpringApplication.run(SpringBootFirstWebApplication.class, args); 
 	}
 
 }
